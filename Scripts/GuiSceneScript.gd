f73acdb858b7d86684bb4f7b64a77186dcf1d9a3
Adam Sawicki <sawickiap@poczta.onet.pl>
2026-01-30 23:45:24 +0100

    Added GuiScene

---
+++ Scripts/GuiSceneScript.gd
@@ -0,0 +1,31 @@
+class_name GuiScene extends Node2D
+
+var health_bar_max_width: int = 0
+
+func _ready() -> void:
+	health_bar_max_width = %InteriorLife.size.x
+	# TEMP FOR TESTING
+	var tween := create_tween()
+	tween.set_loops() # infinite
+	tween.tween_method(
+	func(v: float) -> void:
+		update_health(v),
+		0.0,
+		1.0,
+		3.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_method(
+	func(v: float) -> void:
+		update_health(v),
+		1.0,
+		0.0,
+		3.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+
+func _process(delta: float) -> void:
+	pass
+
+func update_health(percent: float) -> void:
+	if percent <= 0.01:
+		%InteriorLife.visible = false
+	else:
+		%InteriorLife.visible = true
+		%InteriorLife.size.x = percent * health_bar_max_width
