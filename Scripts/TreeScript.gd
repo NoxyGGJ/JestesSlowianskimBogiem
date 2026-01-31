150d5e628fd868d4780e94bead12a81e033a59f4
Adam Sawicki <sawickiap@poczta.onet.pl>
2026-01-31 17:21:30 +0100

    Disabling tree collision in MaskMode=3

--- Scripts/TreeScript.gd
+++ Scripts/TreeScript.gd
@@ -4,3 +4,4 @@ func update_mask() -> void:
 	var meshes_node = %Meshes
 	for i in meshes_node.get_child_count():
 		meshes_node.get_child(i).visible = GlobalObject.CurrentMask == i
+	%StaticBody3D.process_mode = Node.PROCESS_MODE_INHERIT if GlobalObject.CurrentMask != 3 else Node.PROCESS_MODE_DISABLED
