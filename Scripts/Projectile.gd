bdb55be35a4cac681690a2f1a9cc70055e714b2e
Krystian <szlagor.k@onet.pl>
2026-01-31 03:22:50 +0100

    Changed projectile presentation

--- Scripts/Projectile.gd
+++ Scripts/Projectile.gd
@@ -2,6 +2,14 @@ extends Area3D
 class_name Projectile
 
 @export var speed : float = 2
+var mesh
+var particles
+var timer
+
+func _ready() -> void:
+	mesh = $base
+	particles = $particles
+	timer = $Timer
 
 func _physics_process(delta: float) -> void:
 	position += delta * speed * transform.basis[2]
@@ -10,5 +18,12 @@ func _on_body_entered(body: Node3D) -> void:
 	var enemy = body is Enemy
 	if enemy:
 		body.Damage()
+	timer.start()	
+
+	if mesh.is_queued_for_deletion():
+		mesh.queue_free()
 		
+	particles.emitting = false
+
+func _on_timer_timeout() -> void:
 	queue_free()
