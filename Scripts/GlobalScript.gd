498d7d1096e07c69bfed69c4d84cf7c9446dd673
Adam Sawicki <sawickiap@poczta.onet.pl>
2026-02-01 00:02:24 +0100

    Added method get_finished_totem_count

--- Scripts/GlobalScript.gd
+++ Scripts/GlobalScript.gd
@@ -16,3 +16,11 @@ func _process(delta: float) -> void:
 
 func get_totems_status() -> Array[bool]:
 	return [firstTotemFinished, secondTotemFinished, thirdTotemFinished, foruthTotemFinished]
+
+func get_finished_totem_count() -> int:
+	var res := 0
+	res += 1 if firstTotemFinished else 0
+	res += 1 if secondTotemFinished else 0
+	res += 1 if thirdTotemFinished else 0
+	res += 1 if foruthTotemFinished else 0
+	return res
