8cb5e7074d6fbbad94694a448ee9ac11c8ce3a9d
Krystian <szlagor.k@onet.pl>
2026-01-31 04:31:33 +0100

    Added charged attack mode

--- Scripts/Projectile.gd
+++ Scripts/Projectile.gd
@@ -2,9 +2,20 @@ extends Area3D
 class_name Projectile
 
 @export var speed : float = 2
+@export var max_scale : Vector3
+@export var speed_scaling : float
 var mesh
 var particles
 var timer
+var is_scaling : bool = true;
+var time : float
+
+
+func start_scaling() -> void:
+	is_scaling = true;
+
+func release() -> void:
+	is_scaling = false;
 
 func _ready() -> void:
 	mesh = $base
@@ -12,9 +23,17 @@ func _ready() -> void:
 	timer = $Timer
 
 func _physics_process(delta: float) -> void:
-	position += delta * speed * transform.basis[2]
+	if is_scaling:
+		time += delta * speed_scaling
+		scale = scale.lerp(max_scale, time)
+		
+	else:
+		position += delta * speed * transform.basis[2]
 
 func _on_body_entered(body: Node3D) -> void:
+	if is_scaling:
+		return
+	
 	var enemy = body is Enemy
 	if enemy:
 		body.Damage()
